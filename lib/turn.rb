6a77268829f2ae4f57dd3df1788c631254c47667
yossi katzman <issoyk@gmail.com>
2018-05-29 18:03:18 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def display_board(arr)
+  puts  " #{arr[0]} | #{arr[1]} | #{arr[2]} "
+  puts "-----------"
+  puts  " #{arr[3]} | #{arr[4]} | #{arr[5]} "
+  puts "-----------"
+  puts  " #{arr[6]} | #{arr[7]} | #{arr[8]} "
+end
